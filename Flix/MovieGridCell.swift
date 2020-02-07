f4853d0ab25c29e09de7fd1225c618f3b653d368
Apnambir Sandhu <assandhu317@gmail.com>
2020-02-06 18:06:08 -0800

    Stuff

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//
+//
+//  Created by Apnambir Sandhu on 2/5/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
